4d811f4bb18712b8d8fe2fed3c96606bf52bcb9f
Wei <billcuitop@gmail.com>
2024-06-22 12:24:38 -0400

    argument naming refactor

--- models/decoder.py
+++ models/decoder.py
@@ -3,43 +3,40 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def construct_vae_decoder(hidden_dims, latent_dim, encoder_conv_out_size):
+def construct_vae_decoder(conv_channels, latent_dim, encoder_conv_out_size):
     decoder_conv_list = []
-    decoder_fc = nn.Linear(latent_dim, hidden_dims[-1] * encoder_conv_out_size**2)
+    decoder_fc = nn.Linear(latent_dim, conv_channels[-1] * encoder_conv_out_size**2)
     # reverse the convolutional layer channels, don't alter the outside list
-    hidden_dims = hidden_dims.copy()
-    hidden_dims.reverse()
-    for i in range(len(hidden_dims) - 1):
+    conv_channels = conv_channels.copy()
+    conv_channels.reverse()
+    for i in range(len(conv_channels) - 1):
         decoder_conv_list.append(
             nn.Sequential(
                 nn.ConvTranspose2d(
-                    in_channels=hidden_dims[i], 
-                    out_channels=hidden_dims[i+1],
+                    in_channels=conv_channels[i], 
+                    out_channels=conv_channels[i+1],
                     kernel_size=3, 
                     stride=2, 
                     padding=1,
                     output_padding=1),
-                nn.BatchNorm2d(hidden_dims[i+1]),
+                nn.BatchNorm2d(conv_channels[i+1]),
                 nn.LeakyReLU())
         )
     # final convolution layer
     decoder_conv_list.append( 
         nn.Sequential(
-            nn.ConvTranspose2d(hidden_dims[-1],
-                               hidden_dims[-1],
+            nn.ConvTranspose2d(conv_channels[-1],
+                               conv_channels[-1],
                                kernel_size=3,
                                stride=2,
                                padding=1,
                                output_padding=1),
-            nn.BatchNorm2d(hidden_dims[-1]),
+            nn.BatchNorm2d(conv_channels[-1]),
             nn.LeakyReLU(),
-            nn.Conv2d(hidden_dims[-1], out_channels=3,
+            nn.Conv2d(in_channels=conv_channels[-1], out_channels=3,
                       kernel_size=3, padding=1),
             nn.Sigmoid())
         )    
     decoder_conv_lyrs = nn.Sequential(*decoder_conv_list)
     
     return decoder_fc, decoder_conv_lyrs
-
-if __name__ == "__main__":
-    decoder_fc , decoder_conv_lyrs = construct_vae_decoder(latent_dim=256, encoder_conv_out_size=14)
--- models/encoder.py
+++ models/encoder.py
@@ -4,10 +4,10 @@ import torch.nn.functional as F
 import numpy as np
 
 
-def construct_vae_encoder(hidden_dims, latent_dim):
+def construct_vae_encoder(conv_channels, latent_dim):
     in_dim = 3 
     encoder_conv_list = []
-    for h_dim in hidden_dims:
+    for h_dim in conv_channels:
         encoder_conv_list.append(
             nn.Sequential(
                 nn.Conv2d(in_channels=in_dim, out_channels=h_dim,
@@ -22,6 +22,6 @@ def construct_vae_encoder(hidden_dims, latent_dim):
     dummy_out = encoder_conv_lyrs(torch.rand(1, 3, 224, 224))
     conv_out_size = dummy_out.shape[2]
     # add in flattened fully connected layers
-    encoder_fc_mu = nn.Linear(hidden_dims[-1] * conv_out_size**2, latent_dim)
-    encoder_fc_var = nn.Linear(hidden_dims[-1] * conv_out_size**2, latent_dim)
+    encoder_fc_mu = nn.Linear(conv_channels[-1] * conv_out_size**2, latent_dim)
+    encoder_fc_var = nn.Linear(conv_channels[-1] * conv_out_size**2, latent_dim)
     return encoder_conv_lyrs, encoder_fc_mu, encoder_fc_var, conv_out_size
